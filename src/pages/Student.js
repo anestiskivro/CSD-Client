98f2ec0eb657e0ed180ca8522200ac3244ca6742
anestiskivro <anestiskivro@gmail.com>
2024-06-22 13:45:03 +0300

    in progress

--- src/pages/Student.js
+++ src/pages/Student.js
@@ -8,37 +8,11 @@ function Student() {
     const { email } = location.state || {};
     const { id } = location.state || {};
     return (
-        <div>
-            <style>
-                {`
-                    @media (max-width: 600px) {
-                        .container {
-                            flex-direction: column;
-                            align-items: center;
-                        }
-                    
-                        .info {
-                            width: 100%;
-                            text-align: center;
-                        }
-                    
-                        .appointment {
-                            width: 100%;
-                        }
-                    }
-                `}
-            </style>
-            <div className="container" style={{ display: 'flex' }}>
-                <div className="info">
-                    <Info email={email} />
-                </div>
-                <div className="appointment">
-                    <Appointment id={id} email={email} />
-                </div>
-            </div>
+        <div style={{ display: 'flex' }}>
+            <Info email={email}></Info>
+            <Appointment id={id} email={email} />
         </div>
     );
-
 }
 
 export default Student

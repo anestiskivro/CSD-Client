23cd7d92f30497b0c9b6230bd642cd96c7c4e222
anestiskivro <anestiskivro@gmail.com>
2024-06-23 17:55:36 +0300

    left and right class for mobile ooo

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,17 @@
 import React from 'react';
+import { useMediaQuery } from 'react-responsive';
 import Info from '../components/info';
+import '../components/info.css';
 import Login from '../components/login';
 function Home() {
+    const isMobile = useMediaQuery({ maxWidth: 768 });
+
     return (
-        <div style={{ display: 'flex' }}>
+        <div className={`home-container ${isMobile ? 'mobile' : 'desktop'}`}>
             <Info />
             <Login />
         </div>
-    )
+    );
 }
 
-export default Home
+export default Home;

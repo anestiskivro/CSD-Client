a22392560047966c009a7d3b3c177627a65f34b2
anestiskivro <anestiskivro@gmail.com>
2024-06-24 11:13:15 +0300

    mobile develop more ads

--- src/components/info.js
+++ src/components/info.js
@@ -1,30 +1,41 @@
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import axios from 'axios';
+import { useMediaQuery } from 'react-responsive';
 import { useNavigate } from 'react-router-dom';
 import Footer from './footer';
 import './info.css';
+
 const Info = ({ email }) => {
   const navigate = useNavigate();
+  const isMobile = useMediaQuery({ query: '(max-width: 768px)' });
+
   const handleSignOut = async () => {
     try {
       await axios.post('https://rendezvous-csd-106ea9dcba7a.herokuapp.com/logout');
       navigate('/');
-    }
-    catch (error) {
+    } catch (error) {
       console.error('Error logging out:', error);
     }
   };
+
   return (
     <div className="left">
       <div className="header">
         <img src="/imagesUOC.png" alt="Logo" className="logo" />
-        <h1>Welcome</h1>
-        <h2>Rendezvous System for CSD Courses</h2>
+        {!isMobile && (!email) && (
+          <>
+            <h1>Welcome</h1>
+            <h2>Rendezvous System for CSD Courses</h2>
+          </>
+        )}
         {email ? (
           <div>
-            <h3><i class="fa-solid fa-user" style={{ paddingRight: '8px' }}></i>User: {email}</h3>
+            <h3>
+              <i className="fa-solid fa-user" style={{ paddingRight: '8px' }}></i>User: {email}
+            </h3>
             <button className="button" onClick={handleSignOut}>
-              <i className="fa-solid fa-sign-out-alt" style={{ paddingRight: '8px' }}></i>Sign Out</button>
+              <i className="fa-solid fa-sign-out-alt" style={{ paddingRight: '8px' }}></i>Sign Out
+            </button>
           </div>
         ) : (
           <h3>This is an application for the Examination of Students</h3>
@@ -33,6 +44,6 @@ const Info = ({ email }) => {
       <Footer />
     </div>
   );
-}
+};
 
 export default Info;

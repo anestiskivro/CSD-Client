9bd754a312207fdeedc388e888f919abe2c786ff
anestiskivro <anestiskivro@gmail.com>
2024-06-27 10:34:33 +0300

    table for mobile again react table!!!

--- src/pages/Review.js
+++ src/pages/Review.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
+import { useTable } from 'react-table';
 import Info from '../components/info';
 import '../components/review.css';
 
@@ -18,7 +19,6 @@ function Review() {
     const [students, setStudents] = useState([]);
     const [TAs, setTAs] = useState([]);
 
-    // Effect to set window width on resize
     useEffect(() => {
         const handleResize = () => {
             setWindowWidth(window.innerWidth);
@@ -102,89 +102,102 @@ function Review() {
         navigate(path, { state: { id, email } });
     };
 
+    const taColumns = React.useMemo(
+        () => [
+            { Header: 'Exam', accessor: 'exam' },
+            { Header: 'Date', accessor: 'date' },
+            { Header: 'AM', accessor: 'am' },
+            { Header: 'FromTime', accessor: 'fromTime' },
+            { Header: 'EndTime', accessor: 'endTime' },
+            { Header: 'Status', accessor: 'status' },
+            { Header: 'Message', accessor: 'message' },
+        ],
+        []
+    );
+
+    const taData = React.useMemo(
+        () => selectedSlots.map((val) => {
+            const matched_exam = exams.find(exam => exam.cid === val.cid && exam.eid === val.eid);
+            const slotAppointment = appointments.find(appointment => appointment.slotId === val.slotid);
+            const student = slotAppointment ? students.find(student => student.id === slotAppointment.studentId) : null;
+            return {
+                exam: matched_exam ? matched_exam.name : '',
+                date: val.date,
+                am: student ? student.student_number : '',
+                fromTime: val.fromTime,
+                endTime: val.endTime,
+                status: val.status,
+                message: student ? <a href={`mailto:${student.email}`}>Send Message</a> : ''
+            };
+        }),
+        [selectedSlots, exams, appointments, students]
+    );
+
+    const studentColumns = React.useMemo(
+        () => [
+            { Header: 'Course', accessor: 'course' },
+            { Header: 'Exam', accessor: 'exam' },
+            { Header: 'TA', accessor: 'ta' },
+            { Header: 'Date', accessor: 'date' },
+            { Header: 'FromTime', accessor: 'fromTime' },
+            { Header: 'EndTime', accessor: 'endTime' },
+        ],
+        []
+    );
+
+    const studentData = React.useMemo(
+        () => selectedAppointments.map((val) => {
+            const matchedCourse = selectedCourses.find(course => course.cid === val.cid);
+            const matchedExam = exams.find(exam => exam.cid === val.cid && exam.eid === val.eid);
+            const matched_TA = TAs.find(ta => ta.taid === val.taid);
+            return {
+                course: matchedCourse ? matchedCourse.code : 'N/A',
+                exam: matchedExam ? matchedExam.name : 'N/A',
+                ta: matched_TA ? matched_TA.lastname : 'N/A',
+                date: val.date,
+                fromTime: val.FromTime,
+                endTime: val.EndTime
+            };
+        }),
+        [selectedAppointments, selectedCourses, exams, TAs]
+    );
+
+    const tableInstance = useTable({
+        columns: id.includes("TA") ? taColumns : studentColumns,
+        data: id.includes("TA") ? taData : studentData
+    });
+
+    const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } = tableInstance;
+
     return (
         <div className={`home-container ${windowWidth <= 428 ? 'mobile' : 'desktop'}`}>
             <Info email={email} />
             <div className="right">
-                {id.includes("TA") ? (
-                    <div className="table-container">
-                        {selectedSlots && selectedSlots.length > 0 ? (
-                            <table>
-                                <thead>
-                                    <tr>
-                                        <th>Exam</th>
-                                        <th>Date</th>
-                                        <th>AM</th>
-                                        <th>FromTime</th>
-                                        <th>EndTime</th>
-                                        <th>Status</th>
-                                        <th>Message</th>
+                <div className="table-container">
+                    <table {...getTableProps()}>
+                        <thead>
+                            {headerGroups.map(headerGroup => (
+                                <tr {...headerGroup.getHeaderGroupProps()}>
+                                    {headerGroup.headers.map(column => (
+                                        <th {...column.getHeaderProps()}>{column.render('Header')}</th>
+                                    ))}
+                                </tr>
+                            ))}
+                        </thead>
+                        <tbody {...getTableBodyProps()}>
+                            {rows.map(row => {
+                                prepareRow(row);
+                                return (
+                                    <tr {...row.getRowProps()}>
+                                        {row.cells.map(cell => (
+                                            <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+                                        ))}
                                     </tr>
-                                </thead>
-                                <tbody>
-                                    {selectedSlots.map((val, i) => {
-                                        const matched_exam = exams.find(exam => exam.cid === val.cid && exam.eid === val.eid);
-                                        const slotAppointment = appointments.find(appointment => appointment.slotId === val.slotid);
-                                        const student = slotAppointment ? students.find(student => student.id === slotAppointment.studentId) : null;
-                                        return (
-                                            <tr key={i}>
-                                                <td>{matched_exam ? matched_exam.name : ''}</td>
-                                                <td>{val.date}</td>
-                                                <td>{student ? student.student_number : ''}</td>
-                                                <td>{val.fromTime}</td>
-                                                <td>{val.EndTime}</td>
-                                                <td>{val.Status}</td>
-                                                <td>
-                                                    {student ? (
-                                                        <a href={`mailto:${student.email}`}>Send Message</a>
-                                                    ) : ''}
-                                                </td>
-                                            </tr>
-                                        );
-                                    })}
-                                </tbody>
-                            </table>
-                        ) : (
-                            <div>No slots available</div>
-                        )}
-                    </div>
-                ) : (
-                    <div className="table-container">
-                        {selectedAppointments && selectedAppointments.length > 0 ? (
-                            <table>
-                                <thead>
-                                    <tr>
-                                        <th>Course</th>
-                                        <th>Exam</th>
-                                        <th>TA</th>
-                                        <th>Date</th>
-                                        <th>FromTime</th>
-                                        <th>EndTime</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    {selectedAppointments.map((val, i) => {
-                                        const matchedCourse = selectedCourses.find(course => course.cid === val.cid);
-                                        const matchedExam = exams.find(exam => exam.cid === val.cid && exam.eid === val.eid);
-                                        const matched_TA = TAs.find(ta => ta.taid === val.taid);
-                                        return (
-                                            <tr key={i}>
-                                                <td>{matchedCourse ? matchedCourse.code : 'N/A'}</td>
-                                                <td>{matchedExam ? matchedExam.name : 'N/A'}</td>
-                                                <td>{matched_TA ? matched_TA.lastname : 'N/A'}</td>
-                                                <td>{val.date}</td>
-                                                <td>{val.FromTime}</td>
-                                                <td>{val.EndTime}</td>
-                                            </tr>
-                                        );
-                                    })}
-                                </tbody>
-                            </table>
-                        ) : (
-                            <div>No appointments available</div>
-                        )}
-                    </div>
-                )}
+                                );
+                            })}
+                        </tbody>
+                    </table>
+                </div>
                 <div className='btn-group3'>
                     <button className="button" onClick={handleBack}>
                         <i className="fa-solid fa-arrow-left" style={{ paddingRight: '8px' }}></i>
